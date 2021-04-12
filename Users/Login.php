fb1baabe91567f46af954ea997d51899860cc227
Lukas L <35036216+lukaslaza@users.noreply.github.com>
2021-04-12 16:58:37 +0200

    Añadimos try catch y mejoramos la respuesta cuando hay errores.

--- Users/Login.php
+++ Users/Login.php
@@ -13,34 +13,62 @@ error_reporting(E_ALL);
 setlocale(LC_ALL, "es_ES");
 date_default_timezone_set('Europe/Madrid');
 ini_set('max_execution_time', 0);
-
 header('Content-type: text/json; charset=utf-8');
-
 
 require_once '/home/esphouse/crmesphouses.com/src/configuration.php';
 require_once '/home/esphouse/crmesphouses.com/src/Users/Users.php';
 
-$User = new Users;
+try {
 
-if (isset($_GET['accessKey'])){
-	$User->__set('accesskey', $_GET['accessKey']);
-}else{
-	exit('err');
+	//Creamos la clase Usuario
+	$User = new Users;
+
+	//Comprobamos que nos hayan pasado el accessKey
+	if (isset($_GET['accessKey'])) {
+		//Lo asignamos a la clase
+		$User->__set('accesskey', $_GET['accessKey']);
+	} else {
+		//Devolvemos excepcion
+		throw new Exception('accessKey not recieved');
+	}
+
+	//Comprobamos que nos hayan pasado el nombre de usuario
+	if (isset($_GET['userName'])) {
+		//Lo asignamos a la clase
+		$User->__set('user_name', $_GET['userName']);
+	} else {
+		//Devolvemos excepcion
+		throw new Exception('userName not recieved');
+	}
+
+	//Probamos a hacer login
+	if (!$User->login(FALSE)) {
+		//Devolvemos excepcion
+		throw new Exception('Cannot login');
+	} else {
+		//Recogemos los datos del usuario para comprobar que el sessionId funcione
+		if ($User->retrieveViaId($User->__get('sessionId'))) {
+			$result = [
+				'success' => TRUE,
+				'message' => 'Logged',
+				'data' => [
+					'id' => $User->__get('id'),
+					'sessionId' => $User->__get('sessionId'),
+				],
+			];
+		} else {
+			//Devolvemos excepcion
+			throw new Exception('SessionId not works');
+		}
+	}
+} catch (Exception $error) {
+	$result = [
+		'success' => FALSE,
+		'message' => $error->getMessage(),
+		'data' => NULL,
+	];
+} finally {
+	header("HTTP/1.1 " . $result['success'] ? '200' : '500' . " " . $result['message']);
+	echo json_encode($result);
 }
-
-if (isset($_GET['user_name'])){
-	$User->__set('user_name', $_GET['user_name']);
-}else{
-	exit('err');
-}
-
-if (!$User->login(false)) {
-	exit('err');
-}
-
-if ($User->retrieveViaId($User->__get('sessionId'))){
-	echo json_encode($User->data);
-}else{
-	exit('err');
-}
-
+?>
